18c607c4dca93cce4fb3c1cc3b6a0aee0d9c3114
GuilhermeGuimaraesDev <guilherme.paiva.ti@gmail.com>
2025-09-13 08:00:32 -0300

    alteração no arquivo create_topics.sh

--- infra/create_topics.sh
+++ infra/create_topics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Script para criar tópicos no Kafka
 
-docker exec -it pdf-processor-kafka-1 kafka-topics --create --topic pdf_incoming --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
-docker exec -it pdf-processor-kafka-1 kafka-topics --create --topic pdf_text --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+docker exec kafka kafka-topics.sh --create --topic pdf_incoming --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1
+docker exec kafka kafka-topics.sh --create --topic pdf_text --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1
+docker exec kafka kafka-topics.sh --create --topic pdf_dead_letter --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1
