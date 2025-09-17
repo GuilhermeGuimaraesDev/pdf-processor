911e11549251df58111e88e89c7f3d4c7a88c4ae
GuilhermeGuimaraesDev <guilherme.paiva.ti@gmail.com>
2025-09-17 12:14:59 -0300

    restruturação api

--- api/main.py
+++ api/main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI, UploadFile
-import shutil, os
-from producer.app import publish_pdf_metadata
+import shutil
+import os
+import json
+import time
+from kafka import KafkaProducer, errors
 
 app = FastAPI()
 
 UPLOAD_DIR = "/data/incoming"
+
+# Configura producer Kafka interno da API
+for i in range(10):  # tenta 10 vezes
+    try:
+        producer = KafkaProducer(bootstrap_servers="kafka:9092")
+        print("Conectado ao Kafka!")
+        break
+    except errors.NoBrokersAvailable:
+        print("Kafka não disponível, tentando novamente...")
+        time.sleep(5)
+else:
+    raise Exception("Não foi possível conectar ao Kafka após várias tentativas.")
+
+def publish_pdf_metadata(pdf_path: str):
+    msg = {"id": os.path.basename(pdf_path), "path": pdf_path}
+    producer.send("pdf_incoming", value=msg)
+    producer.flush()
+    print(f"[API] Publicado: {msg}")
 
 @app.post("/upload")
 async def upload_pdf(file: UploadFile):
@@ -16,8 +37,8 @@ async def upload_pdf(file: UploadFile):
 
 @app.get("/status/{job_id}")
 def get_status(job_id: str):
-    return {"job_id": job_id, "status": "processing/done"}  # Conectar ao DB para status real
+    return {"job_id": job_id, "status": "processing/done"}  # depois conecta no DB
 
 @app.get("/result/{job_id}")
 def get_result(job_id: str):
-    return {"job_id": job_id, "text": "texto extraído"}  # Conectar ao DB para resultado real
+    return {"job_id": job_id, "text": "texto extraído"}  # depois conecta no DB
